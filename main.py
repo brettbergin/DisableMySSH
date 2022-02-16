0b509c5d6f69be4bf5be35894957451237264d03
Brett Bergin <brettberginbc@yahoo.com>
2022-02-15 17:18:32 -0800

    Added types, exceptions, and comments/logging.

--- main.py
+++ main.py
@@ -43,11 +43,11 @@ class Logger(object):
 
 
 class Config(object):
-    def __init__(self, aws_region):
+    def __init__(self, aws_region: str) -> None:
         self.aws_region = aws_region
 
     def _handle_args(self):
-        """ This function parses the CLI argugment, and returns the parsed result. """
+        """This function parses the CLI argugment, and returns the parsed result."""
 
         parser = argparse.ArgumentParser()
         parser.add_argument(
@@ -55,17 +55,17 @@ class Config(object):
         )
         return parser.parse_args()
 
-    def ec2_client(self):
-        """ This function returns an AWS boto3 client object. """
+    def ec2_client(self) -> boto3.client:
+        """This function returns an AWS boto3 client object."""
 
         return boto3.client("ec2", region_name=self.aws_region)
 
-    def ec2_resource(self):
-        """ This function returns an AWS boto3 resource object. """
+    def ec2_resource(self) -> boto3.resource:
+        """This function returns an AWS boto3 resource object."""
 
         return boto3.resource("ec2", region_name=self.aws_region)
 
-    def perform_ssh_remediation(self):
+    def perform_ssh_remediation(self) -> bool:
         """
         This function is called to determine if the client would like to remediate the ssh open ingress rules.
         """
@@ -75,14 +75,14 @@ class Config(object):
 
 
 class AWS(object):
-    def __init__(self, config, logger):
+    def __init__(self, config: Config, logger: Logger) -> None:
         self.config = config
         self.logger = logger
 
         self.ec2_client = self.config.ec2_client()
         self.ec2_resource = self.config.ec2_resource()
 
-    def _check_for_ingress_violations(self, ip_perms):
+    def _check_for_ingress_violations(self, ip_perms: list) -> bool:
         """
         This function iterates over all ip permissions in a group, and evaluates them for the case of 0.0.0.0:22.
         If found, we increment a count from 0. If said count results in a number greater than zero, we return True."""
@@ -90,7 +90,7 @@ class AWS(object):
         violation_count = 0
 
         for perm in ip_perms:
-            
+
             # Only returns and ingress rule that has a 0.0.0.0 ingress.
             ip_ranges = [ip for ip in perm["IpRanges"] if ip["CidrIp"] == "0.0.0.0/0"]
 
@@ -101,15 +101,21 @@ class AWS(object):
 
         return True if violation_count > 0 else False
 
-    def _inspect_instance_security_groups(self, groups):
+    def _inspect_instance_security_groups(self, groups: list) -> list:
         """
         This function iterates over all the security groups attached to an instance, and checks for violations.
-        If found, the security group object is appended into a list of affected groups, and returned. """
+        If found, the security group object is appended into a list of affected groups, and returned."""
 
         group_violations = []
 
         for group_id in groups:
-            sec_group = self.ec2_resource.SecurityGroup(id=group_id)
+            try:
+                sec_group = self.ec2_resource.SecurityGroup(id=group_id)
+            
+            except Exception as err:
+                self.logger.error(f"Unable to create a security group object using SG ID: {group_id}. Error: {err}.")
+                continue
+
             group_has_violations = self._check_for_ingress_violations(
                 ip_perms=sec_group.ip_permissions
             )
@@ -119,31 +125,38 @@ class AWS(object):
 
         return group_violations
 
-    def audit_ec2_instances(self, ssh_disabled):
-        """ This function is the entrypoint to invoke the audit of our ec2 instances and their security groups.
+    def audit_ec2_instances(self, ssh_disabled: bool) -> bool:
+        """This function is the entrypoint to invoke the audit of our ec2 instances and their security groups.
         The function downloads a list of ec2 instances from the AWS API, and evaluates their security group ingress rules.
 
-        If found that the instance has a security group with an ingress rule matching a 0.0.0.0 ingress with a from port of 22, 
+        If found that the instance has a security group with an ingress rule matching a 0.0.0.0 ingress with a from port of 22,
         we will attempt to revoke said ingress.
         """
 
         self.logger.info(f"Security Group ingress revocation: {ssh_disabled}.")
 
-        response = self.ec2_client.describe_instances()
+        try:
+            response = self.ec2_client.describe_instances()
+
+        except Exception as err:
+            self.logger.error(f"Unable to fetch list of AWS EC2 instances. Error: {err}.")
+            return False
+
         reservations = [i["Instances"] for i in response["Reservations"]]
         instances = [
             instance for reservation in reservations for instance in reservation
         ]
 
+        err_count = 0
         for instance in instances:
-            # Returns a list of security groups that are attached to this instance 
+            # Returns a list of security groups that are attached to this instance
             # that have groups with ingress rules that have a CIDR of 0.0.0.0 with a from_port of 22.
             affected_instance_groups = self._inspect_instance_security_groups(
                 groups=[grp["GroupId"] for grp in instance["SecurityGroups"]]
             )
 
             self.logger.debug(
-                f"Instance: {instance['InstanceId']} has {len(affected_instance_groups)} security groups with ingress violations."
+                f"Instance: {instance['InstanceId']} has {len(affected_instance_groups)} security groups with ingress violation(s)."
             )
 
             # If the instance doesnt have any security groups found with ingress violations, we move to the next instance.
@@ -152,22 +165,33 @@ class AWS(object):
 
             # At this point we have found security groups with ingress violations.
             for group in affected_instance_groups:
-                self.logger.info(f"Instance: {instance['InstanceId']} has group: {group.group_name} with an open ssh ingress from 0.0.0.0.")
-                
+                self.logger.info(
+                    f"!!! Found affected instance: {instance['InstanceId']} where group {group.group_name} has port 22 allowed from 0.0.0.0."
+                )
+
                 # If the ssh disable configuration has been set to true, we will revoke the ingress on said group.
                 if ssh_disabled:
                     self.logger.info(f"Revoking Ingress For Group: {group.group_id}.")
-                    group.revoke_ingress(
-                        CidrIp="0.0.0.0/0", FromPort=22, ToPort=22, IpProtocol="tcp"
-                    )
+
+                    try:
+                        group.revoke_ingress(
+                            CidrIp="0.0.0.0/0", FromPort=22, ToPort=22, IpProtocol="tcp"
+                        )
+                    except Exception as err:
+                        self.logger.error(f"Unable to revoke ingress for SG: {group.group_id}. Error: {err}.")
+                        err_count += 1
+            
+        return True if err_count == 0 else False
 
 
-def main():
-    """ This is the main function of the program. It will initialize a logger, config, and aws class. 
-    Finally, it will invoke the audit of ec2 instances. """
+def main() -> None:
+    """This is the main function of the program. It will initialize a
+    logger, config, and aws class. Finally, it will invoke the audit of ec2 instances.
+    """
 
-    logger = Logger(level="DEBUG").setup_logging()
     config = Config(aws_region=os.environ["AWS_REGION"])
+    logger = Logger(level="DEBUG").setup_logging()
+
     aws = AWS(config=config, logger=logger)
 
     logger.info("Starting DisableMySSH Audit.")
